31fa78c67650999bb08b416f62bbc9eb67f3ff38
Gilbert Simon <si_gil@live.fr>
2020-08-11 14:54:59 +0100

    default control

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,5 +14,25 @@ class DefaultController extends AbstractController
     public function index(){
         return $this->render('index.html.twig');
     }
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+
+        $user = new User($username);
+        $user->setPassword($encoder->encodePassword($user, $password));
+
+        $em->persist($user);
+        $em->flush();
+
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+
+    public function api()
+    {
+        return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
+    }
 }
     
